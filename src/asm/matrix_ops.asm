82b0b140f25e6a3d5134ac3560febe982526991f
Ron Asnahon <asnahonron@gmail.com>
2025-05-21 13:09:33 +0800

    Create matrix_ops.asm

---
+++ src/asm/matrix_ops.asm
@@ -0,0 +1,7 @@
+section .text
+global matrix_multiply
+matrix_multiply:
+    ; Placeholder: Matrix multiplication
+    ; Input: rdi (matrix A), rsi (matrix B), rdx (result), rcx (size)
+    mov rax, 0
+    ret
